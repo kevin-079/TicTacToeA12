54c2d3a0ece9507c5cddc25c4655bc425b24564e
YusufAcala <167275609+YusufAcala@users.noreply.github.com>
2024-12-19 12:58:44 +0700

    benerin size board

--- GameSelector.java
+++ GameSelector.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 
 public class GameSelector extends JPanel {
     public GameSelector(JFrame parentFrame) {
-        setLayout(new GridLayout(3, 1, 10, 10)); // Label + 2 Tombol
+        setLayout(new GridLayout(3, 1, 10, 10)); //Label + 2 Tombol
 
         JLabel welcomeLabel = new JLabel("Welcome! Pilih Gamenya: ", JLabel.CENTER);
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
