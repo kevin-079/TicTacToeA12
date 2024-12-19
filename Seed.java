1ee8b38666c937511dca7ac372bc9603fcd40589
kevin-079 <5026231079@student.its.ac.id>
2024-12-19 09:06:13 +0700

    Update Seed.java

--- Seed.java
+++ Seed.java
@@ -15,8 +15,8 @@ import javax.swing.ImageIcon;
  *  however, not supported.
  */
 public enum Seed {   // to save as "Seed.java"
-    CROSS("X", "images/cross.gif"),   // displayName, imageFilename
-    NOUGHT("O", "images/not.gif"),
+    CROSS("X", "images/X.gif"),   // displayName, imageFilename
+    NOUGHT("O", "images/O.gif"),
     NO_SEED(" ", null);
 
     // Private variables
